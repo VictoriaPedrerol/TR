b7cecd2d2de5ffc610e3bf53d727d9edd72d15eb
delfinapedr <2023_delfina.pedrerol@iticbcn.cat>
2024-11-24 22:02:07 +0100

    Modificat codi fractal wave

--- shapes/FractalWave.java
+++ shapes/FractalWave.java
@@ -17,8 +17,8 @@ public class FractalWave implements Shape, Drawable {
     public void draw(Graphics g) {
         // Implement drawing logic for the fractal tree
         drawWave(g, main.getFrame().getWidth() / 2, main.getFrame().getHeight()-50, -180, 12);
-        drawTinyWave(g,main.getFrame().getWidth() / 2 + 300,  main.getFrame().getHeight()+ 10, -180, 11);
-        drawTinyWave2(g, main.getFrame().getWidth() / 2 + 180, main.getFrame().getHeight()+10 , 0, -10, 12);
+        drawTinyWave(g,main.getFrame().getWidth() / 2 + 300,  main.getFrame().getHeight(), -180, 11);
+        drawTinyWave2(g, main.getFrame().getWidth() / 2 + 300, main.getFrame().getHeight()+10 , 0, -10, 12);
     }
 
     private void drawWave(Graphics g, int x1, int y1, double angle, int depth) {
@@ -31,18 +31,18 @@ public class FractalWave implements Shape, Drawable {
     }
     private void drawTinyWave(Graphics g, int x1, int y1, double angle, int depth) {
         if (depth == 0) return;
-        int x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * depth * 5);
-        int y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) * depth * 5);
+        int x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * depth * 10);
+        int y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) * depth * 10);
         g.drawLine(x1, y1, x2, y2);
-        drawTinyWave(g, x2, y2, angle - 15, depth - 1);
-        drawTinyWave(g, x2, y2, angle + 15, depth - 1);
+        drawTinyWave(g, x2, y2, angle - 20, depth - 1);
+        drawTinyWave(g, x2, y2, angle + 20, depth - 1);
     }
     private void drawTinyWave2(Graphics g, int centerX, int centerY, double angle, double rotationAngle, int depth) {
         if (depth == 0) return;
     
         // Longitud de las ramas
-        int waveLengthX = depth * 10; // Componente X
-        int waveLengthY = depth * 5;  // Componente Y
+        int waveLengthX = depth * 8; // Componente X
+        int waveLengthY = depth * 8;  // Componente Y
     
         // Calcular las coordenadas relativas al centro
         int relativeX = (int) (Math.cos(Math.toRadians(angle)) * waveLengthX);
